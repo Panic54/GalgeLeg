8cc679b2168f644273608ddef9a1b41895739512
Mads Finnerup <Finnerup1@gmail.com>
2018-03-13 12:48:27 +0100

    The greatness continues...

--- GalgeLeg/src/game/TestHent.java
+++ GalgeLeg/src/game/TestHent.java
@@ -6,6 +6,7 @@
 package game;
 
 import brugerautorisation.transport.rmi.Brugeradmin;
+import brugerautorisation.transport.rmi.Brugeradminklient;
 import java.rmi.Naming;
 
 /**
@@ -14,8 +15,11 @@ import java.rmi.Naming;
  */
 public class TestHent {
     public static void main (String[] args) throws Exception { 
+      Brugeradminklient hej = new Brugeradminklient();
+      
+      System.out.println(hej.login("s154102", "abc123"));
         
-    Brugeradmin ba =(Brugeradmin) Naming.lookup("rmi://javabog.dk:11114/galgeserver");
-    ba.hentBruger("s154102", "abc123");   
+    //Brugeradmin ba =(Brugeradmin) Naming.lookup("rmi://javabog.dk:11114/galgeserver");
+    //ba.hentBruger("s154102", "abc123");   
     }
 }
